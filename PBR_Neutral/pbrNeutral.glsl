e29ef3ef72267eaf33efa00faae7a16f02342ee3
Emmett Lalish <elalish@google.com>
2024-03-13 08:54:47 -0700

    addressing feedback

--- PBR_Neutral/pbrNeutral.glsl
+++ PBR_Neutral/pbrNeutral.glsl
@@ -1,20 +1,21 @@
-float startCompression = 0.8 - 0.04;
-float desaturation = 0.15;
+// Input color is non-negative and resides in the Linear Rec. 709 color space.
+// Output color is also Linear Rec. 709, but in the [0, 1] range.
 
 vec3 PBRNeutralToneMapping( vec3 color ) {
-  color *= toneMappingExposure;
+  const float startCompression = 0.8 - 0.04;
+  const float desaturation = 0.15;
 
-  float x = min(color.r, min(color.g, color.b));
-  float offset = x < 0.08 ? x - 6.25 * x * x : 0.04;
+  const float x = min(color.r, min(color.g, color.b));
+  const float offset = x < 0.08 ? x - 6.25 * x * x : 0.04;
   color -= offset;
 
-  float peak = max(color.r, max(color.g, color.b));
+  const float peak = max(color.r, max(color.g, color.b));
   if (peak < startCompression) return color;
 
-  float d = 1. - startCompression;
-  float newPeak = 1. - d * d / (peak + d - startCompression);
+  const float d = 1. - startCompression;
+  const float newPeak = 1. - d * d / (peak + d - startCompression);
   color *= newPeak / peak;
 
-  float g = 1. - 1. / (desaturation * (peak - newPeak) + 1.);
+  const float g = 1. - 1. / (desaturation * (peak - newPeak) + 1.);
   return mix(color, vec3(1, 1, 1), g);
 }
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Tone Mappers
 A collection of tone mappers for the display of 3D graphics, specifically for converting HDR linear light from PBR that has orders of magnitude larger range than the best HDR TVs, down to SDR or other display device output ranges.
 
-## PBR Neutral
+## [PBR Neutral](PBR_Neutral)
 A tone mapper designed specifically for PBR color accuracy, to get sRGB colors in the output render that match as faithfully as possible the input sRGB baseColor under gray-scale lighting. This is aimed toward product photography use cases, where the scene is well-exposed and HDR color values are mostly restricted to small specular highlights. 
